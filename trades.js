8bfb47ad2b87eba9f9a0a609ca2488e702cf92a8
JamesWRC <38713144+JamesWRC@users.noreply.github.com>
2022-03-24 10:38:28 +1100

    pushing config

--- trades.js
+++ trades.js
@@ -1,9 +1,38 @@
+// const trades = [
+// 	{
+// 		asset: "ETH", // Asset you want to buy
+// 		currency: "BTC", // Currency you want to buy the asset with
+// 		quoteOrderQty: 0.1, // Buy 0.1 BTC worth of ETH
+//		schedule:: "0 11 * * *" // Everyday at 11am
+// 	}
+// ];
 const trades = [
+	// Daily DCA
 	{
 		asset: "ETH", // Asset you want to buy
-		currency: "BTC", // Currency you want to buy the asset with
-		quoteOrderQty: 0.1, // Buy 0.1 BTC worth of ETH
-	}
+		currency: "AUD", // Currency you want to buy the asset with
+		quoteOrderQty: 15, // Buy $10 AUD worth of ETH
+		schedule: '* * * * *' // Every day @ 11am
+	},
+	// Weekly DCA
+	{
+		asset: "USDT", // Asset you want to buy
+		currency: "AUD", // Currency you want to buy the asset with
+		quoteOrderQty: 30, // Buy ~$10 AUD worth of USDT
+		schedule: '0 10 * * 4' // At 10:00 on Thursday
+	},
+	{
+		asset: "IMX", // Asset you want to buy
+		currency: "USDT", // Currency you want to buy the asset with
+		quoteOrderQty: 10, // Buy $10 USDT worth of IMX
+		schedule: '0 11 * * 4*' // At 11:00 on Thursday
+	},
+	{
+		asset: "LRC", // Asset you want to buy
+		currency: "USDT", // Currency you want to buy the asset with
+		quoteOrderQty: 10, // Buy $10 USDT worth of IMX
+		schedule: '0 11 * * 4*' // At 11:00 on Thursday
+	},
 ];
 
 export { trades };
